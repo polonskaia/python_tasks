98be479f5a3abf70000ff27862759229eb326fdf
Olga Polonskaia <necheuhina.olya@gmail.com>
2026-02-09 20:06:26 +0200

    Add classes task1

---
+++ classes-and-objects/Task1.py
@@ -0,0 +1,17 @@
+class Polygon:
+    pass
+
+class Rectangle(Polygon):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+    
+length = float(input('Enter the length of the rectangle: '))
+width = float(input('Enter the width of the rectangle: '))
+    
+rect = Rectangle(length, width)
+
+print(f'Rectangle area is: {rect.area()}')
